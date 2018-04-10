f36106409692afde5bd6459f4388a03214d9d7b7
Josh <j.spigelman@gmail.com>
2018-04-09 21:15:31 -0400

    started work on C# version of this program

---
+++ ProfileGen/ProfileGen/sqlConnector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ProfileGen
+{
+    class sqlConnector
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //constructor
+        public sqlConnector()
+        {
+            server = "localhost";
+            database = "profile_gen";
+
+        }
+
+    }
+}
